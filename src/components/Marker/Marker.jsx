963667c2db665b400f1d99eddfa5c9687090b2d3
Konrad Kalemba <konrad_kal@hotmail.com>
2020-03-08 23:25:12 +0100

    Marker refactoring

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { useStyletron } from 'baseui';
 
-export default function Marker() {
+export default function Marker({ size, count }) {
   const [, theme] = useStyletron();
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
+      width={size || '32'}
+      height={size || '32'}
       viewBox="0 0 32 32"
-      fill={theme.colors.negative}
-      stroke={theme.colors.negative}
     >
       <circle
         cx="50%"
         cy="50%"
         r="8px"
+        fill={theme.colors.negative}
       ></circle>
       <circle
-        class="pulse"
+        className="pulse"
         cx="50%"
         cy="50%"
         r="12px"
+        stroke={theme.colors.negative}
       ></circle>
+      {count && (
+        <text className="text" x="50%" y="50%" fontSize={'60%'} fill="white" textAnchor="middle" dy=".3em">{count}</text>
+      )}
     </svg>
   )
 }
