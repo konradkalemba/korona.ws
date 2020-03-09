ed5a7544bfbac1cb89975e1f6fb4b7cd12eb06f1
Konrad Kalemba <konrad_kal@hotmail.com>
2020-03-09 14:21:11 +0100

    Implement chart - count per city

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Map } from './Map/Map';
 export { default as StyledCard } from './StyledCard/StyledCard';
 export { default as Marker } from './Marker/Marker';
+export { default as CitiesChart } from './CitiesChart/CitiesChart';
+export { default as DetailsElement } from './DetailsElement/DetailsElement';
