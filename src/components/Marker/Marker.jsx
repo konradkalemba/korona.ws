ec13a9500d6a77bd39575218c3c7362ac93cb34c
Konrad Kalemba <konrad_kal@hotmail.com>
2020-03-12 13:00:17 +0100

    Add basic deaths tracking

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useStyletron } from 'baseui';
 
-export default function Marker({ size, count }) {
+export default function Marker({ size, casesCount, deathsCount }) {
   const [, theme] = useStyletron();
+  const offset = (deathsCount / casesCount) * 100;
 
   return (
     <svg
@@ -12,15 +13,24 @@ export default function Marker({ size, count }) {
       height={size || '32'}
       viewBox="0 0 32 32"
     >
+      {deathsCount && 
+        <defs>
+          <linearGradient id="combined" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset={`${100 - offset}%`} stopColor={theme.colors.negative} />
+            <stop offset={`${offset}%`}  stopColor={theme.colors.primary} />
+          </linearGradient>
+        </defs>
+      }
+
       <circle
         className="pulse"
         cx="50%"
         cy="50%"
         r="8px"
-        fill={theme.colors.negative}
+        fill={deathsCount ? "url('#combined')" : theme.colors.negative}
       ></circle>
-      {count && (
-        <text className="text" x="50%" y="50%" fontSize={'60%'} fill="white" textAnchor="middle" dy=".3em">{count}</text>
+      {casesCount && (
+        <text className="text" x="50%" y="50%" fontSize={'60%'} fill="white" textAnchor="middle" dy=".3em">{casesCount}</text>
       )}
     </svg>
   )
