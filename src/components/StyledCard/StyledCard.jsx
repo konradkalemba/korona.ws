56c73c53488c96bf6d1dc55fc5459a712dd70e24
Konrad Kostępski <kkostepski@users.noreply.github.com>
2020-03-16 21:20:47 +0100

    Restore box-sizing property

--- src/components/StyledCard/StyledCard.jsx
+++ src/components/StyledCard/StyledCard.jsx
@@ -10,6 +10,7 @@ export default function StyledCard({ children, style, ...props }) {
             const styleOverrides = style ? style($theme) : {};
             return {
               width: '100%',
+              boxSizing: 'border-box',
               borderRadius: $theme.borders.radius200,
               boxShadow: $theme.lighting.shadow500,
               ...styleOverrides
