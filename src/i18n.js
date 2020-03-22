7751d4bc1afd03959eb8b0e578628a21a149110c
Maksym Akymenko <maksym.akymenko@revolut.com>
2020-03-22 15:51:48 +0100

    Feat: Add i18n, react-i18n

---
+++ src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslation from "./translations/en.json";
+import plTranslation from "./translations/pl.json";
+
+const resources = {
+  en: {
+    translation: enTranslation
+  },
+  pl: {
+    translation: plTranslation
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: localStorage.getItem("lang") || "pl",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
