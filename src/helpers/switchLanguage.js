34aacfe38b34dede3b1a50e32a4a1eebc804a71d
Maksym Akymenko <maksym.akymenko@revolut.com>
2020-03-22 15:52:17 +0100

    Feat: add switch language functions

---
+++ src/helpers/switchLanguage.js
@@ -0,0 +1,13 @@
+const saveLanguageToStorage = lang => {
+  localStorage.setItem("lang", lang);
+};
+
+export const switchLanguage = ({ i18n }) => {
+  if (i18n.language === "en") {
+    i18n.changeLanguage("pl");
+    saveLanguageToStorage("pl");
+  } else {
+    i18n.changeLanguage("en");
+    saveLanguageToStorage("en");
+  }
+};
