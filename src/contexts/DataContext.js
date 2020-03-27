b905009c20962e91e95ceaeea612fa4c4c358cd2
Marcos Bérgamo <marcos@thedon.com.br>
2020-03-27 20:39:23 +0100

    feat: Refactoring firebase import only necessary deps This removes the console.warn from firebase

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,43 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import firebase from 'firebase';
 
+import { firebase } from '../helpers/firebase';
 import { useConnection } from './ConnectionContext';
-
-const {
-  REACT_APP_FIREBASE_API_KEY,
-  REACT_APP_FIREBASE_AUTH_DOMAIN,
-  REACT_APP_FIREBASE_DATABASE_URL,
-  REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STORAGE_BUCKET,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID,
-  REACT_APP_FIREBASE_MEASUREMENT_ID,
-} = process.env;
-
-firebase.initializeApp({
-  ...(REACT_APP_FIREBASE_API_KEY && { apiKey: REACT_APP_FIREBASE_API_KEY }),
-  ...(REACT_APP_FIREBASE_AUTH_DOMAIN && {
-    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  }),
-  ...(REACT_APP_FIREBASE_DATABASE_URL && {
-    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
-  }),
-  ...(REACT_APP_FIREBASE_PROJECT_ID && {
-    projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  }),
-  ...(REACT_APP_FIREBASE_STORAGE_BUCKET && {
-    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  }),
-  ...(REACT_APP_FIREBASE_MESSAGING_SENDER_ID && {
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  }),
-  ...(REACT_APP_FIREBASE_APP_ID && { appId: REACT_APP_FIREBASE_APP_ID }),
-  ...(REACT_APP_FIREBASE_MEASUREMENT_ID && {
-    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
-  }),
-});
-
-firebase.analytics();
 
 const updatedAtDatabaseRef = firebase.database().ref('/updatedAt');
 const rootDatabaseRef = firebase.database().ref('/');
